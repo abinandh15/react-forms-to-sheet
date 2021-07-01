47798d7c86723c54b17a3b9370f0200296c3e2ce
Abinandh <abinandh15@gmail.com>
2021-07-01 17:46:53 +0530

    feature: form component and css reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import FormComponent from './components/FormComponent';
 
 function App() {
   return (
     <div className="App">
-     
+      <FormComponent></FormComponent>
     </div>
   );
 }
---
+++ src/components/FormComponent.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import './FormComponent-style.scss'
+
+const FormComponent = () => {
+    const detailsForm = useFormik({
+        initialValues:{
+            name: '',
+            email: ''
+        },onSubmit: values=>{
+            console.log(values)
+        }
+    })
+    return (
+        <form onSubmit={detailsForm.handleSubmit} className="DetailsForm">
+            <label htmlFor="name" className="mt-2">Name</label>
+            <input type="text" id="name" name="name" className="mt-1" onChange={detailsForm.handleChange}></input>
+            <label htmlFor="email" className="mt-2">Email</label>
+            <input type="email" id="email" name="email"  className="mt-1" onChange={detailsForm.handleChange}></input>
+            <button type="submit" className="mt-3 submit-button">Submit</button>
+        </form>
+    )
+}
+
+export default FormComponent
