215d7283a1b875c07691b1996048706ecd380f3f
Abinandh <abinandh15@gmail.com>
2021-07-01 23:06:46 +0530

    feature: google sheets integration

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useFormik } from 'formik'
 import './FormComponent-style.scss'
+import appeadRowToSpreadsheet from '../GoogleSheetsService'
 
 const FormComponent = () => {
     const detailsForm = useFormik({
         initialValues:{
             name: '',
             email: ''
-        },onSubmit: values=>{
+        },onSubmit: values=>{ 
             console.log(values)
+            appeadRowToSpreadsheet(values)
         }
     })
     return (
